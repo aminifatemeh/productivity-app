0201bf4ae80c68675d826a2c6c605dc0cf7e909b
Aminifatemeh <aminifatemeh1382@gmail.com>
2025-05-02 21:25:59 +0330

    feat: create user profile card component

---
+++ src/components/ProfileSnippet.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./ProfileSnippet.scss"
+
+function ProfileSnippet(props) {
+    return (
+      <div className="profile-snippet">
+        <div className="profile-snippet__info">
+          <h5>{props.username}</h5>
+          <span>{props.userstatus}</span>
+        </div>
+        <div className="profile-snippet__avatar">
+          <img
+            src="/assets/images/profile-snippet-avatar.svg"
+            alt="profile avatar"
+          />
+        </div>
+      </div>
+    );
+}
+
+export default ProfileSnippet;
